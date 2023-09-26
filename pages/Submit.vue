c5e65c08e89c7315c295ec2d53b6313f80aecf08
reemabahussain <reemabahussain@outlook.com>
2023-09-26 15:40:18 +0300

    submit page

---
+++ pages/Submit.vue
@@ -0,0 +1,20 @@
+<template>
+
+<div class="container py-5">
+
+       <div>
+         <h2 class="text-center py-5"><a class="link-offset-3 text-decoration-none">شكرا لك</a></h2>
+      </div>
+      <div>
+         <h3 class="text-center pb-5"><a class="text-black text-decoration-none">شكرا لك، لقد تم استلام طلبك<br>.سوف نتصل بك قريبا جدا</a></h3>
+      </div>
+
+</div>
+
+</template>
+
+<script>
+export default {
+    name: 'Submit'
+}
+</script>
